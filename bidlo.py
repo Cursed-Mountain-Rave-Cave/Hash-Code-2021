157944e94ca65c36f4962d8265cd26b7dd700a45
Aleksey Semenov <43957407+borgishmorg@users.noreply.github.com>
2021-02-25 21:55:45 +0200

    refactor bidlo

--- bidlo.py
+++ bidlo.py
@@ -1,41 +1,40 @@
 from math import gcd
 from functools import reduce
-
-d, i, s, v, f = map(int, input().split())
-
-intersections = [[] for _ in range(i)]
-roads = {}
-
-for _ in range(s):
-    b, e, name, l = input().split()
-    intersections[int(e)].append(name)
-
-for _ in range(v):
-    route = input().split()
-    for _, street in enumerate(route[1:]):
-        if street not in roads:
-            roads[street] = 0
-        roads[street] += 1
+from dataset import Route, Street, Dataset
 
 
-out_data = {}
+def bidlo(dataset: Dataset):
+    intersections = [[] for _ in range(dataset.intersections_count)]
+    roads = {}
 
-#TODO
-#sort by num of cars 
+    for street in dataset.streets.values():
+        intersections[street.end].append(street.name)
 
-for i, intersection in enumerate(intersections):
-    route = []
-    for road in set(intersection):
-        if road in roads:
-            route.append((road, roads[road]))
-    if len(route) > 0:
-        out_data[i] = route
+    for route in dataset.car_routes:
+        for street in route.streets:
+            if street not in roads:
+                roads[street] = 0
+            roads[street] += 1
 
-print(len(out_data))
 
-for key, value in out_data.items():
-    print(key)
-    print(len(value))
-    c_gcd = reduce(gcd, map(lambda x: x[1], value))
-    for street, count in value:
-        print(street, count // c_gcd)
+    out_data = {}
+
+    #TODO
+    #sort by num of cars 
+
+    for i, intersection in enumerate(intersections):
+        route = []
+        for road in set(intersection):
+            if road in roads:
+                route.append((road, roads[road]))
+        if len(route) > 0:
+            out_data[i] = route
+
+    print(len(out_data))
+
+    for key, value in out_data.items():
+        print(key)
+        print(len(value))
+        c_gcd = reduce(gcd, map(lambda x: x[1], value))
+        for street, count in value:
+            print(street, count // c_gcd)
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import sys
 from dataset import Street, Route, Dataset
+from bidlo import bidlo
 
+original_stdout = sys.stdout # Save a reference to the original standard output
 
 if __name__ == "__main__":
     for filename in sys.argv[1:]:
         dataset = Dataset(filename)
-        print(dataset.__dict__)
+        with open(filename.replace('data', 'outputs'), 'w') as f:
+            sys.stdout = f # Change the standard output to the file we created.
+            bidlo(dataset)
+            sys.stdout = original_stdout # Reset the standard output to its original value
