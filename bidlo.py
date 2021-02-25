edce796d19c7ac63ce3f1f32765331d5b88e940e
Your Name <you@example.com>
2021-02-25 21:38:30 +0200

    add new version of bidlo

--- bidlo.py
+++ bidlo.py
@@ -1,18 +1,41 @@
+from math import gcd
+from functools import reduce
+
 d, i, s, v, f = map(int, input().split())
 
-roads = [[] for _ in range(i)]
+intersections = [[] for _ in range(i)]
+roads = {}
 
 for _ in range(s):
     b, e, name, l = input().split()
-    roads[int(e)].append(name)
+    intersections[int(e)].append(name)
 
 for _ in range(v):
-    input()
+    route = input().split()
+    for _, street in enumerate(route[1:]):
+        if street not in roads:
+            roads[street] = 0
+        roads[street] += 1
 
-print(len(roads))
 
-for i, roads in enumerate(roads):
-    print(i)
-    print(len(roads))
-    for road in set(roads):
-        print(road, 1)
+out_data = {}
+
+#TODO
+#sort by num of cars 
+
+for i, intersection in enumerate(intersections):
+    route = []
+    for road in set(intersection):
+        if road in roads:
+            route.append((road, roads[road]))
+    if len(route) > 0:
+        out_data[i] = route
+
+print(len(out_data))
+
+for key, value in out_data.items():
+    print(key)
+    print(len(value))
+    c_gcd = reduce(gcd, map(lambda x: x[1], value))
+    for street, count in value:
+        print(street, count // c_gcd)
